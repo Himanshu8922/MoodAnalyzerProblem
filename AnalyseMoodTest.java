ffb4139bf851305d6b3a042c0a77deda6699f99b
Himanshu8922 <himanshurai9211@gmail.com>
2022-10-03 00:16:30 +0530

    test case to return Invalid Mood using custom exception

---
+++ AnalyseMoodTest.java
@@ -0,0 +1,17 @@
+package moodAnalyzerProgram;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnalyseMoodTest {
+	
+	@Test
+	public void messageShouldBeInvalidMood() {
+		AnalyseMood testmood = new AnalyseMood(null);
+		try {
+			testmood.moodAnalyser();
+		} catch (Exception e) {
+			Assert.assertEquals("Invalid Mood", e.getMessage());
+		}
+	}
+
+}
